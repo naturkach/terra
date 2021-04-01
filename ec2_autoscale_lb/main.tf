637e8adc1d3868f42a48c4bb785ac3319afd84d5
EC2 Default User <ec2-user@ip-172-31-21-139.eu-north-1.compute.internal>
2021-04-01 11:50:52 +0000

    added tf

---
+++ ec2_autoscale_lb/main.tf
@@ -0,0 +1,71 @@
+https://blog.gruntwork.io/an-introduction-to-terraform-f17df9c6d180
+provider "aws"{
+  region = "eu-central-1"
+}
+
+#Get Linux AMI ID using SSM Parameter endpoint in our region
+data "aws_ssm_parameter" "LinuxAmi" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0767046d1677be5a0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#output "public_ip" {
+#  value       = aws_instance.example.public_ip
+#  description = "The public IP of the web server"
+#}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones   = data.aws_availability_zones.all.names
+
+  min_size = 1
+  max_size = 3
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  availability_zones = data.aws_availability_zones.all.names  # This adds a listener for incoming HTTP requests.
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = var.server_port
+    instance_protocol = "http"
+  }
+}
